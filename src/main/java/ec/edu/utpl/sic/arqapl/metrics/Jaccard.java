75756d55f8207ec99b5af16db3bfff67ad1476de
cristhianA94 <cristhianapolo.94@gmail.om>
2019-11-06 22:50:17 -0500

    algoritmo JaroWinkler

--- src/main/java/ec/edu/utpl/sic/arqapl/metrics/Jaccard.java
+++ src/main/java/ec/edu/utpl/sic/arqapl/metrics/Jaccard.java
@@ -1,14 +1,13 @@
 package ec.edu.utpl.sic.arqapl.metrics;
 
-import smile.math.distance.JaccardDistance;
+import com.github.vickumar1981.stringdistance.util.StringDistance;
 
 public class Jaccard implements Similaritable {
+    
     @Override
     public double similarity(String text1, String text2) {
-        var jaccard = new JaccardDistance<String>();
-        var similarity = 1.0 - jaccard.d(text1.split("\\s"),
-                text2.split("\\s"));
-
+        double similarity = StringDistance.jaccard(text1, text2);
+        System.out.printf("La distancia Jaccard es: %.5f", similarity);
         return similarity;
     }
 }
--- src/main/java/ec/edu/utpl/sic/arqapl/metrics/JaroWinkler.java
+++ src/main/java/ec/edu/utpl/sic/arqapl/metrics/JaroWinkler.java
@@ -2,13 +2,12 @@ package ec.edu.utpl.sic.arqapl.metrics;
 
 import com.github.vickumar1981.stringdistance.util.StringDistance;
 
-public class Cosine implements Similaritable {
+public class JaroWinkler implements Similaritable {
 
     @Override
     public double similarity(String text1, String text2) {
-        double similarity = StringDistance.cosine(text1, text2);
-        System.out.printf("La distancia de Cosino es: %.5f", similarity);
-
+        double similarity = StringDistance.jaroWinkler(text1, text2);
+        System.out.printf("La distancia JaroWinkler es: %.5f", similarity);
         return similarity;
     }
 }
